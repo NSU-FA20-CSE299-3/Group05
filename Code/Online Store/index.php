fd8fce17d403d390dc0294fe2d50292ce6815e62
Prantik <hasan.prantik@northsouth.edu>
2020-12-20 22:57:13 +0600

    Enhancement #8
    Here menu bars created in top of navbars with html

--- Code/Online Store/index.php
+++ Code/Online Store/index.php
@@ -62,7 +62,82 @@
                               <i class="fa fa-search"></i>
                               
                         </button>
-                  </div>
+                  </div><div class="navbar-collapse collapse" id="navigation">
+                        <div class="padding-nav">
+                              <ul class="nav navbar-nav navbar-left">
+                                    <li class="active">
+                                          <a href="index.php">Home</a>
+                                          
+                                    </li>
+                                    <li>
+                                          <a href="shop.php">Store</a>
+                                    </li>
+                                    <li>
+                                          <a href="checkout.php">My Account</a>
+                                    </li>
+                                    <li>
+                                          <a href="cart.php">Buying Items</a>
+                                    </li>
+                                    <li>
+                                          <a href="about.php">About Us</a>
+                                    </li>
+                                    <li>
+                                          <a href="services.php">Services</a>
+                                    </li>
+                                    <li>
+                                          <a href="contactus.php"> Contact Us</a>
+                                    </li>
+                                    
+                                    
+                              </ul>
+                              
+                        </div> <!-------Pdding-nav start------>
+                        <a href="cart.php" class="btn btn-primary navbar-btn right">
+                            <i class="fa fa-shopping-cart"></i>
+                            <samp>4 items In Cart</samp>
+                      </a>
+                      <div class="navbar-collapse collapse-right">
+                            <button class="btn navbar-btn btn-primary" type="button" data-toggle="cpllapse" data-target="#search">
+                              <span class="sr-only"> Toggle Search</span>
+                              <i class="fa fa-search"></i>
+                  <div class="navbar-collapse collapse" id="navigation">
+                        <div class="padding-nav">
+                              <ul class="nav navbar-nav navbar-left">
+                                    <li class="active">
+                                          <a href="index.php">Home</a>
+                                          
+                                    </li>
+                                    <li>
+                                          <a href="shop.php">Store</a>
+                                    </li>
+                                    <li>
+                                          <a href="checkout.php">My Account</a>
+                                    </li>
+                                    <li>
+                                          <a href="cart.php">Buying Items</a>
+                                    </li>
+                                    <li>
+                                          <a href="about.php">About Us</a>
+                                    </li>
+                                    <li>
+                                          <a href="services.php">Services</a>
+                                    </li>
+                                    <li>
+                                          <a href="contactus.php"> Contact Us</a>
+                                    </li>
+                                    
+                                    
+                              </ul>
+                              
+                        </div> <!-------Pdding-nav start------>
+                        <a href="cart.php" class="btn btn-primary navbar-btn right">
+                            <i class="fa fa-shopping-cart"></i>
+                            <samp>4 items In Cart</samp>
+                      </a>
+                      <div class="navbar-collapse collapse-right">
+                            <button class="btn navbar-btn btn-primary" type="button" data-toggle="cpllapse" data-target="#search">
+                              <span class="sr-only"> Toggle Search</span>
+                              <i class="fa fa-search"></i>
                   
             </div>
              

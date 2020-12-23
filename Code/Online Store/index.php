d579a3ae8042dea085a12a1d311d83d5c0efba47
Prantik <hasan.prantik@northsouth.edu>
2020-12-23 23:14:30 +0600

    Enhancement #12
    some rewrite code in html(index) and css

--- Code/Online Store/index.php
+++ Code/Online Store/index.php
@@ -2,54 +2,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Online Art Store</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <title>Online Art Store</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
       <link rel="stylesheet" href="styles/style.css">
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 
 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	</head>
+      </head>
 <body>
-	<div id="top">   <!---top bar start--->
+      <div id="top">   <!---top bar start--->
       
       <div class="container">
-      	<div class="col-md-6 offer">
-      		<a href="#" class="btn btn-success btn-sm">
+            <div class="col-md-6 offer">
+                  <a href="#" class="btn btn-success btn-sm">
 
-      			Welcome Guest
-      		</a>
+                        Welcome Guest
+                  </a>
 
-      		<a href="#">Buying Items Total Price: BDT	 100, Total Items 2</a>
-      		
-      	</div>
-      	<div class="col-md-6">
-      		<ul class="menu">
-      			<li>
-      				<a href="customer_registration.php"> Register</a>
-      			</li>
+                  <a href="#">Buying Items Total Price: BDT  100, Total Items 2</a>
+                  
+            </div>
+            <div class="col-md-6">
+                  <ul class="menu">
+                        <li>
+                              <a href="customer_registration.php"> Register</a>
+                        </li>
 
-      			<li>
-      				<a href="checkout.php"> My Account</a>
-      			</li>
-      			
-      			<li>
-      				<a href="cart.php"> Go to cart</a>
-      			</li>
-      			<li>
-      				<a href="login.php"> Login</a>
-      			</li>
-      		</ul>
-      		
-      	</div>
-		
-		</div>
-	</div>     <!-----top bar end---->
+                        <li>
+                              <a href="checkout.php"> My Account</a>
+                        </li>
+                        
+                        <li>
+                              <a href="cart.php"> Go to cart</a>
+                        </li>
+                        <li>
+                              <a href="login.php"> Login</a>
+                        </li>
+                  </ul>
+                  
+            </div>
+            
+            </div>
+      </div>     <!-----top bar end---->
       <div class="navbar navbar-default" id="navbar">
             <div class="container">
                   <div class="navbar-header">
                         <a class="navbar-brand home" href="index.php">
-                              <img src="images/Logo.png" alt ="teehosting" class="hidden-xs">
+                              <img src="images/logo.png" alt ="Arts & Crafts" class="hidden-xs">
+                              <img src="images/small.png" alt="Arts & Crafts" class="visible-xs">
                               
                         </a>
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
@@ -91,61 +92,29 @@
                                     
                               </ul>
                               
+                  
+
+                 
                         </div> <!-------Pdding-nav start------>
                         <a href="cart.php" class="btn btn-primary navbar-btn right">
                             <i class="fa fa-shopping-cart"></i>
                             <samp>4 items In Cart</samp>
                       </a>
                       <div class="navbar-collapse collapse-right">
-                            <button class="btn navbar-btn btn-primary" type="button" data-toggle="cpllapse" data-target="#search">
-                              <span class="sr-only"> Toggle Search</span>
-                              <i class="fa fa-search"></i>
-                  <div class="navbar-collapse collapse" id="navigation">
-                        <div class="padding-nav">
-                              <ul class="nav navbar-nav navbar-left">
-                                    <li class="active">
-                                          <a href="index.php">Home</a>
-                                          
-                                    </li>
-                                    <li>
-                                          <a href="shop.php">Store</a>
-                                    </li>
-                                    <li>
-                                          <a href="checkout.php">My Account</a>
-                                    </li>
-                                    <li>
-                                          <a href="cart.php">Buying Items</a>
-                                    </li>
-                                    <li>
-                                          <a href="about.php">About Us</a>
-                                    </li>
-                                    <li>
-                                          <a href="services.php">Services</a>
-                                    </li>
-                                    <li>
-                                          <a href="contactus.php"> Contact Us</a>
-                                    </li>
-                                    
-                                    
-                              </ul>
-                              
-                        </div> <!-------Pdding-nav start------>
-                        <a href="cart.php" class="btn btn-primary navbar-btn right">
-                            <i class="fa fa-shopping-cart"></i>
-                            <samp>4 items In Cart</samp>
-                      </a>
-                      <div class="navbar-collapse collapse-right">
-                            <button class="btn navbar-btn btn-primary" type="button" data-toggle="cpllapse" data-target="#search">
+                            <button class="btn navbar-btn btn-primary" type="button" data-toggle="collapse" data-target="#search">
                               <span class="sr-only"> Toggle Search</span>
                               <i class="fa fa-search"></i>
                         </button>
                   
             </div>
+
              <div class="collapse clearfix" id="search">
 
                         <form class="navbar-form" method="get" action="result.php">
                               <div class="input-group">
+
                                     <input type="text" name="user_query" placeholder="Search" class="form-control" required="">
+                                    <span class="input-group-btn">
                                     <button type="submit" value="Search" name="search" class="btn btn-primary">
                                           <i class="fa fa-search"></i>
                                            </button>
@@ -161,6 +130,52 @@
                   
             </div>
              
+      </div>
+      <div class ="container" id="slider">
+            <div class="col-md-12">
+                  <div class="carousel slide" id="myCarousel" data-ride="carousel">    <!-----carousel slider stert------>
+                           
+                           <ol class="carousel-indicators">   
+
+                              <li data-target="myCarousel" data-slide-to="0" class="action"></li>
+                               <li data-target="myCarousel" data-slide-to="1"></li>
+                               <li data-target="myCarousel" data-slide-to="2"></li>
+                                <li data-target="myCarousel" data-slide-to="3"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                              <div class="item active">
+                                    <img src="admin_area/slider_images/sl1.png">
+                                    
+                              </div>
+                               <div class="item">
+                                    <img src="admin_area/slider_images/sl2.jpg">
+                                    
+                              </div>
+                               <div class="item">
+                                    <img src="admin_area/slider_images/sl3.jpg">
+                                    
+                              </div>
+                              <div class="item">
+                                    <img src="admin_area/slider_images/sl4.jpg">
+                                    
+                              </div>
+                              
+                        </div>
+                        
+                        <a href="#myCarousel" class="left carousel-control" data-slide="prev">
+                              <span class="glyphicon glyphicon-chevron-left"></span>
+                              <span class="sr-only">Previous</span>
+                        </a>
+                        <a href="#myCarousel" class="right carousel-control" data-slide="next">
+                              <span class="glyphicon glyphicon-chevron-right"></span>
+                              <span class="sr-only">Next</span>
+                        </a>
+
+                        
+                  </div>
+                  
+            </div>
+            
       </div>
                                           
                                    
@@ -218,7 +233,7 @@
       </div>
       
       
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
              
 
 
@@ -230,4 +245,4 @@
 
 </body>
 </html>
-	
+      

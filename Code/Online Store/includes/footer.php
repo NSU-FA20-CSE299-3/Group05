6ae5bd46044bcf6f41d3b45e5e0013d68d4cf3eb
Prantik <hasan.prantik@northsouth.edu>
2020-12-31 01:28:52 +0600

    Enhancement #14
    Creating footer in html and styling in css

--- Code/Online Store/includes/footer.php
+++ Code/Online Store/includes/footer.php
@@ -1,1 +1,79 @@
-footer
+<!----<div id="footer">
+	<div class="container">
+      <div class="row">
+      	<div class="col-md-3 col-sm-6">
+      		<h4>Pages</h4>
+      		<ul>
+      			<li><a href="cart.php">Shopping Cart</a></li>
+      			<li><a href="contact.php">Contact Us</a></li>
+      			<li><a href="shop.php">Shop</a></li>
+      			<li><a href="checkout.php">My Account</a></li>
+      		</ul>
+      		<hr>
+      		<h4>User section</h4>
+
+      		<ul>
+      			<li><a href="checkout.php">Login</a></li>
+      			<li><a href="customer_registration.php">Register</a></li>
+      		</ul>
+      		    <hr class="hidden-md hidden-lg hidden-sm">
+      		    
+      	</div>
+      	<div class="col-md-3 col-sm-6">
+      		<h4>Top Product Categories</h4>
+      		<ul>
+      			<li><a href="#">Bengali Arts & Crafts</a></li>
+      			<li><a href="#">Ancient Europe</a></li>
+      			<li><a href="#">Modern</a></li>
+      			<li><a href="#">Post Modern</a></li>
+      			<li><a href="#">Digital Arts</a></li>
+      		</ul>
+      		<hr class="hidden-md hidden-lg">
+      		
+      	</div>
+      	<div class="col-md-3 col-sm-6">
+      		<h4>Where to find us</h4>
+      		<p>
+      			<strong>Teehosting.com</strong>
+      			<br>Bashundhara
+      			<br> Dhaka
+      			<br>Bangladesh
+      			<br> mahbubhasam@gmail.com
+      			<br> 0171******
+      		</p>
+      		<a href="contact.php">Go to contact us page</a>
+      		<hr class="hidden-md hidden-lg">
+      	</div>
+      	<div class="col-md-3 col-sm-6">
+      		<h4>Get the news</h4>
+      		<p class="text-muted">
+      			Subscribe here for getting news of Arts & Crafts
+      			
+      		</p>
+      		<form action="" method="post">
+      			<div class="input-group">
+      				<input type="text" name="email" class="form-control">
+      				<span class="input-group-btn">
+      					<input type="submit" class="btn btn-default" value="subscribe">
+      					
+      				</span>
+      				
+      			</div>
+      			
+      		</form>
+      		<hr>
+      		<h4>Stay In Touch</h4>
+      		<p class="Social">
+      			<a href="#"><i class="fa fa-facebook"></i></a>
+      			<a href="#"><i class="fa fa-twiter"></i></a>
+      			<a href="#"><i class="fa fa-instagram"></i></a>
+      			<a href="#"><i class="fa fa-google-plus"></i></a>
+      			<a href="#"><i class="fa fa-envelope"></i></a>
+      		</p>
+      		
+      	</div>
+      	
+      </div>
+	</div>
+	
+</div>

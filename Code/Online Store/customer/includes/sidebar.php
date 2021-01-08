35c4dd9704680af25c4a55f95b086df266924bd2
Prantik <hasan.prantik@northsouth.edu>
2021-01-08 23:37:08 +0600

    Enhancement #20
    make sidebar in my account page

--- Code/Online Store/customer/includes/sidebar.php
+++ Code/Online Store/customer/includes/sidebar.php
@@ -1,43 +1,20 @@
 <div class="panel panel-default sidebar-menu">
-	<div class="panel-heading">
-		<h3 class="panel-title">Categories</h3>
+	<div class="panel-heading"><!----------------------Panel heading start------------>
+		<center>
+			<img src="customer_images/self-portrait.jpg" class="img-responsive">
+		</center>
+		<br>
+		<h3 align="center" class="panel-title">Name: Vincent van Gogh</h3>
 		
-	</div>
+	</div><!----------------------Panel heading end--------------->
+
 	<div class="panel-body">
-		<ul class="nav nav-pills nav-stacked category-menu">
-		<li><a href="store.php">Historic</a></li>
-
-		<li><a href="store.php">Post Modern</a></li>
-
-		<li><a href="store.php">Modern</a></li>
-
-		<li><a href="store.php">Digital</a></li>
-
-		<li><a href="store.php">Crafts</a></li>
-	</ul>
-		
-	</div>
-	
-</div>
-
-
-<div class="panel panel-default sidebar-menu">
-	<div class="panel-heading">
-		<h3 class="panel-title">Artists</h3>
-		
-	</div>
-	<div class="panel-body">
-		<ul class="nav nav-pills nav-stacked category-menu">
-		<li><a href="store.php">Zainul Abedin</a></li>
-
-		<li><a href="store.php">SM Sultan</a></li>
-
-		<li><a href="store.php">Vincent van Gogh</a></li>
-
-		<li><a href="store.php">Alexander Cabanel</a></li>
-
-		<li><a href="store.php">Pablo Picasso</a></li>
-	</ul>
+		<ul class="nav nav-pills nav-stacked">
+			<li>
+				<a href="my_account.php?my_order">My Order</a>
+			</li>
+			
+		</ul>
 		
 	</div>
 	

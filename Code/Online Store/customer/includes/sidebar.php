896b6b29f6d21691e8dcda4803b631eeb0cc1abf
Prantik <hasan.prantik@northsouth.edu>
2021-01-09 21:51:08 +0600

    Bug #20
    the my order option is not working

--- Code/Online Store/customer/includes/sidebar.php
+++ Code/Online Store/customer/includes/sidebar.php
@@ -1,21 +1,31 @@
 <div class="panel panel-default sidebar-menu">
-	<div class="panel-heading"><!----------------------Panel heading start------------>
+	<div class="panel-heading">
 		<center>
 			<img src="customer_images/self-portrait.jpg" class="img-responsive">
 		</center>
 		<br>
 		<h3 align="center" class="panel-title">Name: Vincent van Gogh</h3>
 		
-	</div><!----------------------Panel heading end--------------->
-
-	<div class="panel-body">
-		<ul class="nav nav-pills nav-stacked">
-			<li>
-				<a href="my_account.php?my_order">My Order</a>
-			</li>
-			
-		</ul>
-		
 	</div>
+
+    <div class="panel-body">
+    	<ul class="nav nav-pills nav-stacked">
+    		<li class="<?php if(isset($_GET[my_order])){echo"active";}?>">
+    			<a href="my_account.php?my_order">My Order</a>
+    		</li>
+    		<li>
+    			<a href="my_account.php?pay_offline">
+    				<i class="fa fa-bolt"></i>Pay Offline
+    			</a>
+    		</li>
+    		
+    	</ul>
+    	
+    </div>
 	
+
+
+
 </div>
+
+

24a29ff7b7eb245ad7e1f74b0e0d2d9440959be4
Prantik <hasan.prantik@northsouth.edu>
2021-01-10 23:33:15 +0600

    Enhancement #20
    creating my order box in sidebar.php

--- Code/Online Store/customer/my_order.php
+++ Code/Online Store/customer/my_order.php
@@ -6,7 +6,61 @@
 		My Order
 		
 	</h1>
-	<p>If you have any questions, please feel free to <a href="../contactus.php">contact us, our customer service center is working for you 24/7.
+	<p>If you have any questions, please feel free to <a href="../contactus.php">contact us</a>, our customer service center is working for you 24/7.
 	</p>
 </center>
+<hr>
+<div class="table-responsive">
+	<table class="table table-bordered table-hover">
+		<thead>
+			<tr>
+				<th>Sr.No</th>
+				<th>Due Amount</th>
+				<th>Invoice Number</th>
+				<th>Quantity</th>
+				<th>Size</th>
+				<th>Order Data</th>
+				<th>Paid/Unpaid</th>
+				<th>Status</th>
+
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>#1</td>
+				<td>BDT 500</td>
+				<td>11111</td>
+				<td>2</td>
+				<td>LARGE</td>
+				<td>Unpaid</td>
+				<td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">Confirm If Paid</a></td>
+
+			</tr>
+			<tr>
+				<td>#2</td>
+				<td>BDT 500</td>
+				<td>11111</td>
+				<td>2</td>
+				<td>LARGE</td>
+				<td>Unpaid</td>
+				<td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">Confirm If Paid</a></td>
+
+			</tr>
+			<tr>
+				<td>#3</td>
+				<td>BDT 500</td>
+				<td>11111</td>
+				<td>2</td>
+				<td>LARGE</td>
+				<td>Unpaid</td>
+				<td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">Confirm If Paid</a></td>
+
+			</tr>
+			
+		</tbody>
+	</table>
+	
+</div>
+
+
 </div>

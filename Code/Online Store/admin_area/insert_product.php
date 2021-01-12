b5fde78be8fd389888170322620eaf56958c99da
Prantik <hasan.prantik@northsouth.edu>
2021-01-13 04:35:31 +0600

    Enhancement #22
    Inserting Data to Database using php & mysql

--- Code/Online Store/admin_area/insert_product.php
+++ Code/Online Store/admin_area/insert_product.php
@@ -1,8 +1,17 @@
+<?php
+include("includes/db.php");
+
+
+?>
+
+
 <!DOCTYPE html>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Insert Product</title>
+	<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>tinymce.init({selector:'textarea'});</script>
 	<meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -14,7 +23,7 @@
 </head>
 <body>
 
-	<div class="row">
+	<div class="row">   <!---------------breadcrumb row start-------->
 		<div class="col-lg-12">
 			<div class="breadcrumb">
 				<li class="active">
@@ -27,6 +36,124 @@
 			
 		</div>
 		
+	</div><!---------------breadcrumb row end-------->
+	<div class="row">
+		<div class="col-lg-3">
+			
+		</div>
+		<div class="col-lg-6">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+						<i class="fa fa-money fa-fw"></i>Insert product
+					</h3>
+				</div>
+                <div class="panel-body">
+                	<form class="form-horizontal" method="post" action="" enctype="multipart/form-data">
+                		
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Title
+                			</label>
+                			<input type="text" name="product_title" class="form-control" required="">
+                			
+                		</div>
+
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Artists
+                			</label>
+                			<select name="artists" class="form-control">
+                				<option> Select an artist</option>
+
+                				<?php
+                                 $get_artists="select * from artists";
+                                 $run_artists=mysqli_query($con,$get_artists);
+                                 while($row=mysqli_fetch_array($run_artists)){
+                                 	$id=$row['Artists_id'];
+                                 	$artists_name=$row['Artists_name'];
+                                 	echo "<option value='id' >$artists_name </option>";
+                                 }
+                				?>
+                			</select>
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Categories
+                			</label>
+                			<select name="cat" class="form-control">
+                				<option>Select A Category</option>
+                				<?php
+                                 $get_cats="select * from categories";
+                                 $run_cats=mysqli_query($con,$get_cats);
+                                 while($row=mysqli_fetch_array($run_cats)){
+                                 	$cat_id=$row['cat_id'];
+                                 	$cat_title=$row['cat_title'];
+                                 	echo "<option value='cat_id' >$cat_title </option>";
+                                 }
+                				?>
+                			</select>
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Image 1
+                			</label>
+                			<input type="file" name="product_img1" class="form-control" required="">
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Image 2
+                			</label>
+                			<input type="file" name="product_img2" class="form-control" required="">
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Image 3
+                			</label>
+                			<input type="file" name="product_img3" class="form-control" required="">
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Price
+                			</label>
+                			<input type="text" name="product_price" class="form-control" required="">
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Keyword
+                			</label>
+                			<input type="text" name="product_keyword" class="form-control" required="">
+                			
+                		</div>
+                		<div class="form-group">
+                			<label class="col-md-3 control-label">
+                				Product Description
+                			</label>
+                			<textarea name="product_desc" class="form-control" rows="6" cols="19"></textarea>
+                			
+                		</div>
+                		<div class="form-group">
+                			<input type="submit" name="submit" value="Insert Product" class="btn btn-primary form-control">
+                			
+                		</div>
+                		
+                	</form>
+                	
+                </div>
+				
+			</div>
+			
+		</div>
+		<div class="col-lg-3">
+			
+		</div>
+		
 	</div>
 
 
@@ -35,3 +162,37 @@
 
 </body>
 </html>
+
+<?php
+if(isset($_POST['submit'])){
+	$product_title=$_POST['product_title'];
+	$artists=$_POST['artists'];
+	$cat=$_POST['cat'];
+	$product_img1=$_FILES['product_img1']['name'];
+	$product_img2=$_FILES['product_img2']['name'];
+	$product_img3=$_FILES['product_img3']['name'];
+	$product_price=$_POST['product_price'];
+	$product_desc=$_POST['product_desc'];
+	$product_keyword=$_POST['product_keyword'];
+
+	$temp_name1=$_FILES['product_img1']['tmp_name'];
+	$temp_name2=$_FILES['product_img2']['tmp_name'];
+	$temp_name3=$_FILES['product_img3']['tmp_name'];
+
+	move_uploaded_file($temp_name1, "product_images/check/$product_img1");
+	move_uploaded_file($temp_name2, "product_images/check/$product_img2");
+	move_uploaded_file($temp_name3, "product_images/check/$product_img3");
+
+
+	$insert_product="insert into products(p_cat_id, cat_id, date, product_title, product_img1, product_img2, product_img3, product_price, product_desc, product_keyword) values('$artists','$cat',NOW(),'$product_title','$product_img1','$product_img2','$product_img3','$product_price','$product_desc', '$product_keyword')";
+
+	$run_product=mysqli_query($con, $insert_product);
+	if($run_product){
+		echo"<script>alert('Product Inserted Successfully')</script>"; 
+		echo"<script>window.open('insert_product.php')</script>";
+	}
+
+}
+
+
+?>
